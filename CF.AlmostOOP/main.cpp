1ff4f8acd0f54e9f803ba3bad5007d619c65a669
Alan Sampson <alansamps@gmail.com>
2021-05-24 14:33:36 -0700

    Initial Commit

---
+++ CF.AlmostOOP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CF.AlmostOOP
+//
+//  Created by Alan Sampson on 5/24/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
